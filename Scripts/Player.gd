d0fd9da81338cd0b237e746a3d488f748c460739
Konshift <calovassy@gmail.com>
2024-07-18 15:18:52 -0500

    Shadow Shaders
    Added shadowy shaders

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -2,12 +2,9 @@ extends CharacterBody2D
 
 @export var speed = 100
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
 func _physics_process(delta):
 	handleMove()
+	move_and_slide()
 
 func handleMove():
 	var dir = Input.get_vector("move_left", "move_right", "move_up", "move_down")
