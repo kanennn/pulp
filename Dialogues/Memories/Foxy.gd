35acb1625930145c19b04f5f0186cc7d384241b0
Konshift <calovassy@gmail.com>
2024-07-30 17:25:52 -0500

    Memory
    - Added sprite for memory.
    - Organized files.
    - Updated test scene.

---
+++ Dialogues/Memories/Foxy.gd
@@ -0,0 +1,6 @@
+extends Node
+
+																																					# Cut off character length is about here
+static var dialogue = [
+	"The quick, brown fox jumps over the lazy dog. . .",
+	"and eats the dog."]
